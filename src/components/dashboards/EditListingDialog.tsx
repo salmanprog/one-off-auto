bb262c323ac21bc9ea90989237651bde60ea64b5
unknown <salman@chameleon-ideas.com>
2025-08-08 23:17:34 -0600

    show location

--- src/components/dashboards/EditListingDialog.tsx
+++ src/components/dashboards/EditListingDialog.tsx
@@ -1024,6 +1024,7 @@ const EditListingDialog: React.FC<EditListingDialogProps> = ({ isOpen, onClose, 
               <Autocomplete
                                   className="w-full p-3 border border-gray-300 rounded-md"
                                   apiKey="AIzaSyDtGUZeWBavRsRTTzKDgeuje_4iBu1vrWE"
+                                  value={formData.vehicle_owner_address}
                                   onPlaceSelected={(place) => {
                                     handlePlaceSelected(place)
                                   }}
