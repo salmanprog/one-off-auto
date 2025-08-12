efbd6721eb5ac1f8d866d0f5511491fded22f08d
unknown <salman@chameleon-ideas.com>
2025-08-11 22:29:58 -0600

    show only approved vehicle

--- app/Models/Vehicle.ts
+++ app/Models/Vehicle.ts
@@ -321,14 +321,14 @@ export default class Vehicle extends RestModel
     public static async getVehicleLists(limit:number)
     {
         let show_limit = !_.isEmpty(limit) ? limit : 3;
-        let record = await this.query().preload('vehicleCategory').preload('user').preload('media').preload('VehicleDriverType').preload('VehicleMotorSize').preload('VehicleTransmissionType').preload('VehicleFuelType').preload('VehicleSellerType').preload('VehicleStatus').preload('VehicleSuspensionType').preload('VehicleHpOutRange').preload('VehicleUses').preload('VehicleDocumentation').preload('VehicleMake').preload('VehicleModel').preload('VehicleYear').whereIn('status',['1','2']).orderBy('id','desc').limit(show_limit)
+        let record = await this.query().preload('vehicleCategory').preload('user').preload('media').preload('VehicleDriverType').preload('VehicleMotorSize').preload('VehicleTransmissionType').preload('VehicleFuelType').preload('VehicleSellerType').preload('VehicleStatus').preload('VehicleSuspensionType').preload('VehicleHpOutRange').preload('VehicleUses').preload('VehicleDocumentation').preload('VehicleMake').preload('VehicleModel').preload('VehicleYear').whereIn('status',['1']).orderBy('id','desc').limit(show_limit)
         return record;
     }
 
     public static async getRelatedVehicleLists(slug:string,limit:number)
     {
         let show_limit = !_.isEmpty(limit) ? limit : 9;
-        let record = await this.query().preload('vehicleCategory').preload('user').preload('media').preload('VehicleDriverType').preload('VehicleMotorSize').preload('VehicleTransmissionType').preload('VehicleFuelType').preload('VehicleSellerType').preload('VehicleStatus').preload('VehicleSuspensionType').preload('VehicleHpOutRange').preload('VehicleUses').preload('VehicleDocumentation').preload('VehicleMake').preload('VehicleModel').preload('VehicleYear').whereIn('status',['1','2']).where('slug','<>',slug).orderBy('id','desc').limit(show_limit)
+        let record = await this.query().preload('vehicleCategory').preload('user').preload('media').preload('VehicleDriverType').preload('VehicleMotorSize').preload('VehicleTransmissionType').preload('VehicleFuelType').preload('VehicleSellerType').preload('VehicleStatus').preload('VehicleSuspensionType').preload('VehicleHpOutRange').preload('VehicleUses').preload('VehicleDocumentation').preload('VehicleMake').preload('VehicleModel').preload('VehicleYear').whereIn('status',['1']).where('slug','<>',slug).orderBy('id','desc').limit(show_limit)
         return record;
     }
 
