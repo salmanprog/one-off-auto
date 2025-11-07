0008eea4ebd15c419a6917f239d8dcbd13e38cb9
arsalanahmeddev1 <arsalanahmeddev1@gmail.com>
2025-11-08 04:43:35 +0500

    added clonical links in 2 pages buy sell and listings

--- src/pages/Buy.tsx
+++ src/pages/Buy.tsx
@@ -55,6 +55,7 @@ const Buy = () => {
           name="description"
           content="Buy or discover modified cars listed by owners. Explore custom builds, tuned rides, and connect with sellers passionate about performance cars."
         />
+        <link rel="canonical" href="https://www.oneoffautos.com/buy" />
         <script type="application/ld+json">
           {JSON.stringify(schemaData)}
         </script>
